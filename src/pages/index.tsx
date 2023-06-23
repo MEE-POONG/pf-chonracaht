3c90bb09c2a344788dc33395b63954ca6be56cde
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-06-23 15:56:32 +0700

    pond

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,10 @@
 import React from "react"; 
 
-export default function Home() {
+export default function Index() {
  return(
-  
-  
   <div>
   <h1><span></span><span>MyProfile & Status</span></h1>
-  <h1><span>I'm</span><span><a href="info" className="font-medium"> Mr.chonracaht Intarasut </a></span></h1>
+  <h1><span>I&apos;m</span><span><a href="info" className="font-medium"> Mr.chonracaht Intarasut </a></span></h1>
   <a href="info" className="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-7">ดูข้อมูลเพิ่มเติม</a>
   </div>
   
