b70007457c880f7fed7d926c02ed5cd3a4ad4d09
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-06-23 16:39:41 +0700

    pond

--- components/nav.tsx
+++ components/nav.tsx
@@ -7,18 +7,18 @@ const NavComponent: React.FC<NavProps> = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="http://localhost:3000/">MY Profile </Navbar.Brand>
+        <Navbar.Brand href="index">MY Profile </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="http://localhost:3000/info">Home</Nav.Link>
+            <Nav.Link href="info">Home</Nav.Link>
             <Nav.Link href="profile">ข้อมูล</Nav.Link>
             <NavDropdown title="เพิ่มเติม" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">งานอดิเรก</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Skill</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">สิ่งที่คาดหวัง</NavDropdown.Item>
+              <NavDropdown.Item href="hobby">งานอดิเรก</NavDropdown.Item>
+              <NavDropdown.Item href="skill">Skill</NavDropdown.Item>
+              <NavDropdown.Item href="#">สิ่งที่คาดหวัง</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="http://localhost:3000/contact">ติดต่อ</NavDropdown.Item>
+              <NavDropdown.Item href="contact">ติดต่อ</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
