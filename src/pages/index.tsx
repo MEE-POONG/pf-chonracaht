ce0d6014abc3bff1188398e54f529c393863bd4c
ChonracahtPond <137247404+ChonracahtPond@users.noreply.github.com>
2023-06-23 15:48:04 +0700

    pond

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,6 @@ export default function Home() {
   <h1 className="p-0 m-0 flex..."><span></span><span>MyProfile & Status </span></h1>
   <h1 className=" "><span>I'm</span><span><a href="info" className="font-medium"> Mr.chonracaht Intarasut </a></span></h1>
   <a href="info" className="font-medium text-blue-600 dark:text-blue-500 hover:underline mr-7">ดูข้อมูลเพิ่มเติม</a>
-  
- 
-  
   </div>
   
  )
